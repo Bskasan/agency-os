238141158dd9f0a1c14364055b7df29706da12d9
Bskasan <b.kasan@yandex.com>
2024-06-24 23:25:07 +0300

    up

---
+++ components/navigation/BSTHeader.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const {
+	theme,
+	globals: { title },
+} = useAppConfig();
+
+const {
+	data: navigation,
+	pending,
+	error,
+} = await useAsyncData(
+	'mainNavigation',
+	() => {
+		return useDirectus(
+			readItem('navigation', 'main', {
+				fields: [
+					{
+						items: [
+							'id',
+							'has_children',
+							'title',
+							'icon',
+							'label',
+							'type',
+							'url',
+							{
+								page: ['permalink', 'title'],
+								children: [
+									'id',
+									'title',
+									'has_children',
+									'icon',
+									'label',
+									'type',
+									'url',
+									{
+										page: ['permalink', 'title'],
+									},
+								],
+							},
+						],
+					},
+				],
+			}),
+		);
+	},
+	{
+		transform: (data) => data,
+	},
+);
+</script>
+
+<template>
+	<!-- Header Container -->
+	<!-- Logo Div -->
+	<!-- Menu Item Div and Items -->
+	<!-- Button Group -->
+	<!-- Header Container -->
+</template>
